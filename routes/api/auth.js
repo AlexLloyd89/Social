90d0eaca9b36c797005a2e229fb921048ac69dbe
Alex Lloyd <lloyd.da89@gmail.com>
2019-08-08 09:54:06 -0400

    register user w/jsonWebToken

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../../models/User");
+const auth = require("../../middleware/auth");
 
 //@route GET api/auth
-//@desc test route
-//@access public
-router.get("/", (req, res) => res.send("auth route"));
+//@desc auth route
+//@access private
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("server error");
+  }
+});
 
 module.exports = router;
